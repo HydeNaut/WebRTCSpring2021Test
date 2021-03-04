10b398dde12acd2308b38a0d1925eea7566f8b97
yanyas <hayden.anyasi@network.rca.ac.uk>
2021-03-04 16:02:37 +0100

    Add src folder for event and delegate wrappers
    Update gitignore

---
+++ src/EventDelegateWrappers/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reactive.Linq;
+
+using Microsoft.MixedReality.WebRTC;
+
+namespace EventDelegateWrappers
+{
+
+    public class WebRTCFrameEventArgs : EventArgs
+    {   public byte[] frameBuf = new byte[0];
+        public WebRTCFrameEventArgs(I420AVideoFrame frame)
+        {
+            var width = frame.width;
+            var height = frame.height;
+            var strideY = frame.strideY;
+            var strideU = frame.strideU;
+            var strideV = frame.strideV;
+            var strideA = frame.strideA;
+            frame.CopyTo(frameBuf);
+        }
+    }
+
+    public static class FrameHelper
+    {
+        public static IObservable<WebRTCFrameEventArgs>(PeerConnection pc)
+        {
+            return Observable.FromEvent<>
+        }
+            
+    }
+
+}
